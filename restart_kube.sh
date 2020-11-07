5a5cc6d2f60595131014f51e3b279588582f3ab6
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-11-06 19:16:03 -0500

    Added script to start helm deployment

---
+++ restart_kube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NAMESPACE=$1
+DEPLOYMENT=$2
+
+helm delete $DEPLOYMENT --namespace $NAMESPACE
+helm install $DEPLOYMENT airflow-stable/airflow --version 7.14.0 --namespace $NAMESPACE --values ./custom_values.yaml
+
+sleep 10
+export POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace $NAMESPACE $POD_NAME 8080:8080
