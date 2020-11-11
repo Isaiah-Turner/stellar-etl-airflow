1460282f79f0f006a9c2382c901bc2d7d29ba855
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-11-11 16:48:35 -0500

    Added privileged field

--- restart_kube.sh
+++ restart_kube.sh
@@ -3,7 +3,8 @@ NAMESPACE=$1
 DEPLOYMENT=$2
 
 helm delete $DEPLOYMENT --namespace $NAMESPACE
-helm install $DEPLOYMENT airflow-stable/airflow --version 7.14.0 --namespace $NAMESPACE --values ./custom_values.yaml
+#helm install $DEPLOYMENT airflow-stable/airflow --version 7.14.0 --namespace $NAMESPACE --values ./custom_values.yaml
+helm install $DEPLOYMENT /Users/isaiahturner/Desktop/StellarWorkspace/charts/charts/airflow --namespace $NAMESPACE --values ./custom_values.yaml
 
 sleep 10
 export POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "component=web,app=airflow" -o jsonpath="{.items[0].metadata.name}")
